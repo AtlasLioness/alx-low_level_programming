a92da7cb0729391a284a401097c80390553e6137
AtlasLioness <narjisaliane@gmail.com>
2023-03-16 17:26:55 +0000

    4.Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return string using puts function
+ * @parameter: string is a quote text
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-	/* String initialisation with backlash to keep double quotes */
+
 	char MyStr[] = "\"Programming is like building a multilingual puzzle";
-	/* Usign puts function to write string in stdout 
-	* No need to add newline since puts adds it to the end of argument and publishes it to the output stream */
 
 	puts(MyStr);
-		return(0);
+		return (0);
 }
