36ecc8502054348c90f3266e70c1f4d026309abb
AtlasLioness <narjisaliane@gmail.com>
2023-03-23 18:30:38 +0000

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,18 +11,17 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = size - i; j > 0; j--)
+			for (j = size - i; j >= 1; j--)
 			{
 				_putchar(' ');
 			}
-			for (k = 0; k <= i; k++)
+			for (k = 1; k <= i; k++)
 			{
 				_putchar('#');
 			}
-		_putchar('\n');
-
+			_putchar('\n');
 		}
 	}
 	else
