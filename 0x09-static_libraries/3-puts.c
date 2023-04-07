7bb55306fc9a896cf26bf967285da7ecad8047d7
AtlasLioness <narjisaliane@gmail.com>
2023-04-07 22:32:55 +0000

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,17 @@
-void _puts(char *s)
+#include "main.h"
+/**
+ * _puts - prints string to stdout
+ * @str: string to print
+ *
+ *
+ */
+void _puts(char *str)
 {
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,3 +1,25 @@
-char *_strncpy(char *dest, char *src, int n)
+#include "main.h"
+/**
+ * _strcpy - copies string to a buffer
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer of type char
+ */
+char *_strcpy(char *dest, char *src)
 {
+	int i, counter = 0;
+	char *copied;
+
+	while (src[counter] != '\0')
+	{
+		counter++;
+	}
+	for (i = 0; i <= counter; i++)
+	{
+		dest[i] = src[i];
+	}
+	copied = dest;
+	return (copied);
 }
+
