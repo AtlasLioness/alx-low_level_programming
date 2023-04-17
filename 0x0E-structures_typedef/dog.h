905315bb0821e779fc5fa3459db5725c180fe607
AtlasLioness <narjisaliane@gmail.com>
2023-04-17 15:33:08 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - holds dog's name, age and owner
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* STRUCT_DOG */
