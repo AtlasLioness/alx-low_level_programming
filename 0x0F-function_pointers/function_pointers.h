8f1f2608353b425f0fb63688933bd0fab5fbe1c7
AtlasLioness <narjisaliane@gmail.com>
2023-04-19 14:10:00 +0000

    0. What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,7 +6,6 @@ void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void print_elem(int elem);
-void array_iterator(int *array, size_t size, void (*action)(int));
 void print_elem_hex(int elem);
 int int_index(int *array, int size, int (*cmp)(int));
 int is_98(int elem);
