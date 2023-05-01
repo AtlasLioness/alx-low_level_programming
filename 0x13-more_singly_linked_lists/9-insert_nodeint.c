38c797cee0b9f4034ae6c76038454d9d4bbc3416
AtlasLioness <narjisaliane@gmail.com>
2023-05-01 14:42:05 +0000

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 		i++;
 	}
-	if (tmp == NULL && i != idx)
+	if (tmp == NULL && i < idx)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
