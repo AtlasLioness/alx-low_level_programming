7efa69d3f4f12913c4c23f6910c66b51d5ebb3b1
AtlasLioness <narjisaliane@gmail.com>
2023-05-01 14:53:57 +0000

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,18 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int i = 0;
 
-	if (*head == NULL)
-		return (NULL);
-
-	tmp = *head;
-
-	while (i < idx && tmp != NULL)
-	{
-		previous = tmp;
-		tmp = tmp->next;
-		i++;
-	}
-	if (tmp == NULL && i < idx)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -34,6 +23,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	
+	tmp = *head;
+	while (i < idx && tmp != NULL)
+	{
+		previous = tmp;
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (tmp == NULL && i < idx)
+		return (NULL);
+
 	new_node->next = tmp;
 	previous->next = new_node;
 
