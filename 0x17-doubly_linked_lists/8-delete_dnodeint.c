ffe0e87cd9df6309d0ae899fb58cabe4640f86df
AtlasLioness <narjisaliane@gmail.com>
2023-08-18 00:19:54 +0000

    8. Delete at index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * delete_dnodeint_at_index - deletes node at index of a linked list
+ * @head: address of the list
+ * @index: index of node to be deleted, starts at 0
+ *
+ * Return: 1 if succeeded or -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *where = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(where);
+		return (1);
+	}
+
+	while (where)
+	{
+		if (i == index)
+			break;
+
+		where = where->next;
+		i++;
+
+		if (where == NULL)
+			return (-1);
+	}
+
+	if (where->next == NULL)
+		where->prev->next = NULL;
+	else
+	{
+		where->next->prev = where->prev;
+		where->prev->next = where->next;
+	}
+	free(where);
+	return (1);
+}
